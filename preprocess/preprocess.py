095ec888099708e08b5c363f5b59896c5a34813f
Arafat Khan <anony.masked@hacker.8shield.net>
2024-09-28 16:37:54 -0400

    Add functionality to preprocess 8 spanish words and pickle dump

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -1,5 +1,7 @@
 import librosa
 import numpy as np
+import os
+import pandas as pd
 
 def extract_mfcc(audio_path, n_mfcc=13):
     y, sr = librosa.load(audio_path, sr=None)
@@ -12,10 +14,28 @@ def extract_mel_spectrogram(audio_path, n_mels=128):
     mel_spectrogram_db = librosa.power_to_db(mel_spectrogram)
     return mel_spectrogram_db
 
+def extract_features(audio_dir_path):
 
-audio_path = "data/jugo/common_voice_es_19120092.opus"
-mfcc_features = extract_mfcc(audio_path)
-mel_spectrogram_features = extract_mel_spectrogram(audio_path)
+    data = []
+    words = os.listdir(audio_dir_path)
+
+    for word in words:
+        word_path = os.path.join(audio_dir_path, word)
+        audio_files = os.listdir(word_path)
+
+        for audio_file in audio_files:
+            audio_path = os.path.join(word_path, audio_file)
+            mfcc_features = extract_mfcc(audio_path)
+            data.append((mfcc_features, word))
+
+    df = pd.DataFrame(data, columns=["features", "word"])
+    df.to_pickle("features.pkl")
+
+
+audio_path = "data/"
+
+extract_features(audio_path)
+# mfcc_features = extract_mfcc(audio_path)
+# mel_spectrogram_features = extract_mel_spectrogram(audio_path)
 
 # print(mfcc_features.shape)
-print(mel_spectrogram_features[[128/2, 94/2]])
